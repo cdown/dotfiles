bda51fdd2003a08a6e4a4fc28160a2aa08cae563
Chris Down <chris@chrisdown.name>
2015-07-28 17:55:14 -0700

    ipython: Don't prompt for exit confirmation

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -3,3 +3,4 @@ c = get_config()
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 c.InteractiveShell.colors = 'LightBG'
+c.InteractiveShell.confirm_exit = False
