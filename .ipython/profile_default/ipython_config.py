0fff51d0ca7e27ebad18211497cdf7f7bbeaca62
Chris Down <chris@chrisdown.name>
2015-10-16 20:12:03 +0100

    Enable profilers in iPython

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,6 +1,15 @@
 c = get_config()
 
-c.InteractiveShellApp.extensions = ['autoreload']
+c.TerminalIPythonApp.extensions = [
+    'autoreload',
+    'line_profiler',
+    'memory_profiler',
+]
+c.InteractiveShellApp.extensions = [
+    'autoreload',
+    'line_profiler',
+    'memory_profiler',
+]
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
 c.InteractiveShell.colors = 'LightBG'
 c.InteractiveShell.confirm_exit = False
