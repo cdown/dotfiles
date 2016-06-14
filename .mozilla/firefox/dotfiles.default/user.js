3b992623141151120ee352ccdf21d48c1eda9ed0
Chris Down <chris@chrisdown.name>
2016-06-14 18:07:15 +0100

    firefox: Stop ASI fail

--- .mozilla/firefox/dotfiles.default/user.js
+++ .mozilla/firefox/dotfiles.default/user.js
@@ -1,4 +1,4 @@
-user_pref("browser.tabs.warnOnClose", false)
-user_pref("browser.tabs.warnOnCloseOtherTabs", false)
-user_pref("browser.urlbar.doubleClickSelectsAll", false)
-user_pref("signon.rememberSignons", false)
+user_pref("browser.tabs.warnOnClose", false);
+user_pref("browser.tabs.warnOnCloseOtherTabs", false);
+user_pref("browser.urlbar.doubleClickSelectsAll", false);
+user_pref("signon.rememberSignons", false);
