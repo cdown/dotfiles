2e41e50187a162aeee93d5ee3cbdb4d6ced1f5c1
Chris Down <chris@chrisdown.name>
2017-12-12 08:35:20 +0000

    notify.pl: Don't interpret input as args

--- .irssi/personal/scripts/autorun/notify.pl
+++ .irssi/personal/scripts/autorun/notify.pl
@@ -24,7 +24,7 @@ sub notify {
     }
 
     if ($pid == 0) {
-        system('notify-send', $title, crappy_escape_html($msg));
+        system('notify-send', '--', $title, crappy_escape_html($msg));
         system('if-locked', 'pushover-push', $title, $msg, "1");
         POSIX::_exit(1);
     } else {
