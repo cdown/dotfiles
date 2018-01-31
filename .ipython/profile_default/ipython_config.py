2e54142774d8877e1a15bfbb4987ac09a3c623b4
Chris Down <chris@chrisdown.name>
2018-01-31 17:26:02 +0000

    Revert "ipython: Don't try to run line_profiler"
    This reverts commit 8d13257ea9b051bbcd62a95c0e83fc7f774595f0.

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -2,14 +2,12 @@ c = get_config()
 
 c.TerminalIPythonApp.extensions = [
     'autoreload',
-    # currently not installed due to cython bullshit
-    #'line_profiler',
+    'line_profiler',
     'memory_profiler',
 ]
 c.InteractiveShellApp.extensions = [
     'autoreload',
-    # currently not installed due to cython bullshit
-    #'line_profiler',
+    'line_profiler',
     'memory_profiler',
 ]
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
