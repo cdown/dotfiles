adeea078353099ef854f4f2e2ebdec2820eeed1c
Chris Down <chris@chrisdown.name>
2023-03-07 21:18:18 +0000

    nvim: Set up trouble

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,7 +7,6 @@ local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -29,6 +28,24 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
+
+require('trouble').setup({
+  auto_open = true,
+  auto_close = true,
+
+  -- No icons
+  icons = false,
+  fold_open = "v",
+  fold_closed = ">",
+  indent_lines = false,
+  signs = {
+    error = "E",
+    warning = "W",
+    hint = "H",
+    information = "I"
+  },
+  use_diagnostic_signs = false,
+})
 
 local nvim_lsp = require('lspconfig')
 nvim_lsp.rust_analyzer.setup({
