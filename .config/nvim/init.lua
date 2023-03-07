89bd37b7a5b6e4a5dccf8fabc242625174ca7bc0
Chris Down <chris@chrisdown.name>
2023-03-07 20:23:26 +0000

    neovim: Use packer directly in init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,6 @@
 local vimrc = vim.fn.stdpath("config") .. "/init_.vim"
 vim.cmd.source(vimrc)
+
+require('packer').startup(function(use)
+  use '~/.nvim/bundle/nvim-lspconfig'
+end)
--- .config/nvim/lua/plugins.lua
+++
@@ -1,3 +0,0 @@
-require('packer').startup(function(use)
-  use '~/.nvim/bundle/nvim-lspconfig'
-end)
