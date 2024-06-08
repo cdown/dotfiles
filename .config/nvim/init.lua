5614e894464b7b69a00a6b6b030fed291d05a999
Chris Down <chris@chrisdown.name>
2024-06-08 01:20:04 +0100

    Revert "nvim: trouble: Make work with nvim 0.10"
    For now, we use 2.2.0 again.
    
    This reverts commit 8b4be06a7d20f3be97c47aaa1aadbdd8a4195796.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,21 +50,19 @@ end
 require('trouble').setup({
   auto_open = true,
   auto_close = true,
+
+  -- No icons
+  icons = false,
   fold_open = "v",
   fold_closed = ">",
   indent_lines = false,
-  icons = {
-    indent = {
-      top           = "| ",
-      middle        = "+-",
-      last          = "`-",
-      fold_open     = "> ",
-      fold_closed   = "v ",
-      ws            = "  ",
-    },
-    folder_closed   = "[ ] ",
-    folder_open     = "[>] ",
+  signs = {
+    error = "E",
+    warning = "W",
+    hint = "H",
+    information = "I"
   },
+  use_diagnostic_signs = false,
 })
 
 local nvim_lsp = require('lspconfig')
