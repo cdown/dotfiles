8b4be06a7d20f3be97c47aaa1aadbdd8a4195796
Chris Down <chris@chrisdown.name>
2024-06-08 01:05:32 +0100

    nvim: trouble: Make work with nvim 0.10

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -50,19 +50,21 @@ end
 require('trouble').setup({
   auto_open = true,
   auto_close = true,
-
-  -- No icons
-  icons = false,
   fold_open = "v",
   fold_closed = ">",
   indent_lines = false,
-  signs = {
-    error = "E",
-    warning = "W",
-    hint = "H",
-    information = "I"
+  icons = {
+    indent = {
+      top           = "| ",
+      middle        = "+-",
+      last          = "`-",
+      fold_open     = "> ",
+      fold_closed   = "v ",
+      ws            = "  ",
+    },
+    folder_closed   = "[ ] ",
+    folder_open     = "[>] ",
   },
-  use_diagnostic_signs = false,
 })
 
 local nvim_lsp = require('lspconfig')
